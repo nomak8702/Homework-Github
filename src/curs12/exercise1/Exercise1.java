3ff1a1725aa3e032dd59eac0b0af131a011e2994
Istvan <nomak8702@gmail.com>
2020-10-21 19:48:48 +0300

    Exercise1

--- src/curs12/exercise1/Exercise1.java
+++ src/curs12/exercise1/Exercise1.java
@@ -1,2 +1,38 @@
-package curs12.exercise1;public class Exercise1 {
+package curs12.exercise1;
+
+import java.util.*;
+
+
+public class Exercise1 {
+    public static void main(String[] args) {
+
+        Map<String, Integer> student = new HashMap<>();
+        student.put("Costel Geroge", 8);
+        student.put("Marian Chelaru", 7);
+        student.put("Bob Adrian", 9);
+        student.put("Csabi Szabo", 8);
+
+        for (Map.Entry<String, Integer> result : student.entrySet()) {
+            System.out.println(result.getKey() + " - " + result.getValue());
+        }
+
+        Map<String, Integer> highGrade = new HashMap<>();
+        List<Integer> grades = new ArrayList<>();
+
+        for (Map.Entry<String, Integer> result : student.entrySet()) {
+            if (result.getValue() > 0) {
+                grades.add(result.getValue());
+            }
+        }
+
+        for (Map.Entry<String, Integer> result : student.entrySet()) {
+            if (result.getValue() == Collections.max(grades)) {
+                highGrade.put(result.getKey(), result.getValue());
+            }
+        }
+        System.out.println("------------");
+        for (Map.Entry<String, Integer> result : highGrade.entrySet()) {
+            System.out.println(result.getKey() + " - " + result.getValue());
+        }
+    }
 }
